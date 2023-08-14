b9052d722c76424c374c37c05fb8c365cb36dddc
Brian <wamwangimathenge@gmail.com>
2023-08-14 12:51:39 +0300

    Demonstrate that arrays are iterable
    This commit shows that arrays are iterable and the fact that a for of
    loop can be run on arrays

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // A loop cannot work on an object directly, since an object is not iterable
 
-const car = {
-    speed: 100,
-    color: "blue"
-}
+// const car = {
+//     speed: 100,
+//     color: "blue"
+// }
 
-for(prop of car) {
-    console.log(prop)
-}
+// for(prop of car) {
+//     console.log(prop)
+// }
 
 // Running the above code will throw a TypeError
+
+
+// Arrays are iterable
+const colors = ['red', 'orange', 'yellow']
+for (var color of colors) {
+    console.log(color);
+}
